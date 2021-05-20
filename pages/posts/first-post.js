75e9dc4ece7e537acb027486ce74ea6b5bb15ce7
Lise <lise.carpenter@gmail.com>
2021-05-20 14:29:30 +0700

    styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,25 +1,21 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
 	return (
 		<>
-			<Head>
-				<title>First Post</title>
-			</Head>
-			<h1>First Post</h1>{' '}
-			<Image
-				src='/images/profile.jpg'
-				height={144}
-				width={144}
-				alt='Lise Carpenter'
-			/>
-			<h2>
-				<Link href='/'>
-					<a>Back to home</a>
-				</Link>
-			</h2>
+			<Layout>
+				<Head>
+					<title>First Post</title>
+				</Head>
+				<h1>First Post</h1>
+				<h2>
+					<Link href='/'>
+						<a>Back to home</a>
+					</Link>
+				</h2>
+			</Layout>
 		</>
 	)
 }
