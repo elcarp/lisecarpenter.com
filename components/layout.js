3f2916f0c4f1e30506791eba8359f9c9ea76ead4
Lise <lise.carpenter@gmail.com>
2021-05-21 15:55:08 +0700

    styling hero container

--- components/layout.js
+++ components/layout.js
@@ -9,7 +9,7 @@ export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
 	return (
-		<div className={styles.container}>
+		<div>
 			<Head>
 				<link rel='icon' href='/favicon.ico' />
 				<meta
@@ -26,40 +26,41 @@ export default function Layout({ children, home }) {
 				<meta name='twitter:card' content='summary_large_image' />
 			</Head>
 			<header className={styles.header}>
-				{home ? (
-					<>
-						<Image
-							priority
-							src='/images/profile.jpg'
-							className={utilStyles.borderCircle}
-							height={144}
-							width={144}
-							alt={name}
-						/>
-						<h1 className={utilStyles.heading2Xl}>{name}</h1>
-					</>
-				) : (
-					<>
-						<Link href='/'>
-							<a>
-								<Image
-									priority
-									src='/images/profile.jpg'
-									className={utilStyles.borderCircle}
-									height={108}
-									width={108}
-									alt={name}
-								/>
-							</a>
-						</Link>
-						<h2 className={utilStyles.headingLg}>
-							<Link href='/'>
-								<a className={utilStyles.colorInherit}>{name}</a>
-							</Link>
-						</h2>
-					</>
-				)}
+				<div className='hero'>This is lise, your friend who likes to code</div>
 			</header>
+			{home ? (
+				<>
+					<Image
+						priority
+						src='/images/profile.jpg'
+						className={utilStyles.borderCircle}
+						height={144}
+						width={144}
+						alt={name}
+					/>
+					<h1 className={utilStyles.heading2Xl}>{name}</h1>
+				</>
+			) : (
+				<>
+					<Link href='/'>
+						<a>
+							<Image
+								priority
+								src='/images/profile.jpg'
+								className={utilStyles.borderCircle}
+								height={108}
+								width={108}
+								alt={name}
+							/>
+						</a>
+					</Link>
+					<h2 className={utilStyles.headingLg}>
+						<Link href='/'>
+							<a className={utilStyles.colorInherit}>{name}</a>
+						</Link>
+					</h2>
+				</>
+			)}
 			<main>{children}</main>
 			{!home && (
 				<div className={styles.backToHome}>
