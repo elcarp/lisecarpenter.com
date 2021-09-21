a569bc632eaeba1f8ec40823e4d3ec7b6712cb5c
Lise <lise.carpenter@gmail.com>
2021-09-21 11:34:39 +0700

    updated favicon

--- components/layout.js
+++ components/layout.js
@@ -17,7 +17,7 @@ export default function Layout({ children, home }) {
 	return (
 		<div>
 			<Head>
-				<link rel='icon' href='/favicon.ico' />
+				<link rel='icon' href='/lc-favicon.png' />
 				<meta
 					name='description'
 					content='Learn how to build a personal website using Next.js'
