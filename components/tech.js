21478b9a74ee80d39b7e8fe6b62fc9fc25b2dbd5
Lise <lise.carpenter@gmail.com>
2021-09-21 08:05:49 +0700

    yoga icon

--- components/tech.js
+++ components/tech.js
@@ -1,21 +1,50 @@
 import utilStyles from '../styles/utils.module.css'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
+	faCss3,
+	faHtml5,
+	faJs,
+	faMedium,
 	faReact,
 	faShopify,
 	faWordpress,
 } from '@fortawesome/free-brands-svg-icons'
 
 export default function Tech() {
-	const [isVisible, setVisible] = useState(true)
+	const [isVisible] = useState(true)
 
 	return (
 		<div className='tech-stack bg-grey py-20'>
 			<h1 className={`${utilStyles.heading2Xl} pt-4 text-white text-center`}>
 				Tech that I like
 			</h1>
-			<p>
+			<div className='flex w-1/4 mx-auto'>
+				<FontAwesomeIcon
+					color='#65cccc'
+					icon={faWordpress}
+					width={20}
+					className={`mx-auto pt-6 pb-4 ${
+						isVisible ? 'animate-bounce animation-delay-50 transition' : ''
+					}`}
+				/>
+				<FontAwesomeIcon
+					color='#65cccc'
+					icon={faShopify}
+					width={20}
+					className={`mx-auto pt-6 pb-4 ${
+						isVisible ? 'animate-bounce animation-delay-50 transition' : ''
+					}`}
+				/>
+				<FontAwesomeIcon
+					color='#65cccc'
+					icon={faCss3}
+					width={20}
+					className={`mx-auto pt-6 pb-4 ${
+						isVisible ? 'animate-bounce animation-delay-50 transition' : ''
+					}`}
+				/>
 				<FontAwesomeIcon
 					color='#65cccc'
 					icon={faReact}
@@ -24,10 +53,46 @@ export default function Tech() {
 						isVisible ? 'animate-bounce animation-delay-50 transition' : ''
 					}`}
 				/>
-			</p>
+				<FontAwesomeIcon
+					color='#65cccc'
+					icon={faHtml5}
+					width={20}
+					className={`mx-auto pt-6 pb-4 ${
+						isVisible ? 'animate-bounce animation-delay-50 transition' : ''
+					}`}
+				/>
+				<FontAwesomeIcon
+					color='#65cccc'
+					icon={faJs}
+					width={20}
+					className={`mx-auto pt-6 pb-4 ${
+						isVisible ? 'animate-bounce animation-delay-50 transition' : ''
+					}`}
+				/>
+			</div>
 			<h1 className={`${utilStyles.heading2Xl} pt-4 text-white text-center`}>
 				Other things that I like
 			</h1>
+			<div className='flex w-1/4 mx-auto'>
+				<FontAwesomeIcon
+					color='#65cccc'
+					icon={faMedium}
+					width={20}
+					className={`mx-auto pt-6 pb-4 ${
+						isVisible ? 'animate-bounce animation-delay-50 transition' : ''
+					}`}
+				/>
+				<Image
+					priority
+					src='/images/yoga.svg'
+					height={35}
+					width={35}
+					alt='yoga'
+					className={`mx-auto pt-6 pb-4 ${
+						isVisible ? 'animate-bounce animation-delay-50 transition' : ''
+					}`}
+				/>
+			</div>
 		</div>
 	)
 }
