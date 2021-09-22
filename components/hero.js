5fc756d9e78a6972151ceddb98a98243d7ce2a01
Lise <lise.carpenter@gmail.com>
2021-09-22 16:05:07 +0700

    shiny calendly button

--- components/hero.js
+++ components/hero.js
@@ -1,6 +1,16 @@
 import ReactTypingEffect from 'react-typing-effect'
+import { openPopupWidget } from 'react-calendly'
 
 export default function Hero() {
+	const CustomButton = ({ url, prefill, pageSettings, utm }) => {
+		const onClick = () => openPopupWidget({ url, prefill, pageSettings, utm })
+
+		return (
+			<button onClick={onClick} className='btn shiny'>
+				Let's Chat
+			</button>
+		)
+	}
 	return (
 		<div className='hero flex items-center h-full'>
 			<div className='overlay z-25 bg-black absolute w-full h-full opacity-50'></div>
@@ -12,6 +22,8 @@ export default function Hero() {
 					className='tracking-widest text-sm text-aqua-blue'
 					text={['{`${"Your friend who likes to code"}`} 😊']}
 				/>
+				<CustomButton/>
+				{/* <a class='btn shiny'>Let's Chat</a> */}
 			</div>
 		</div>
 	)
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import Head from 'next/head'
 import Layout from '../components/layout'
 
