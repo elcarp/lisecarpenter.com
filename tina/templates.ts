cfb0ecf6ac9a1b1c3e003e9de44e484e5877ddea
Lise Carpenter <lise.carpenter@gmail.com>
2023-06-07 08:36:27 +0700

    add tina

---
+++ tina/templates.ts
@@ -0,0 +1,10 @@
+import type { TinaField } from "tinacms";
+export function homeFields() {
+  return [
+    {
+      type: "string",
+      name: "test",
+      label: "test",
+    },
+  ] as TinaField[];
+}
